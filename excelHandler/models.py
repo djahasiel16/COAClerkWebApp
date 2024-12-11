89e28e7e5bee3b6e55c7808f406f14f759058d49
djahasiel16 <djahasiel@gmail.com>
2024-12-11 09:46:27 +0800

    Added Accounting Entries & DVRecords Database Models for ASDI & SDN Funds (LFPS, CAPR & COB)

--- excelHandler/models.py
+++ excelHandler/models.py
@@ -1,3 +1,104 @@
 from django.db import models
 
-# Create your models here.
+# ASDI MODELS ----------------------------------------------------------------------------------
+class ASDI_LFPS_DisbursmentVoucherRecord(models.Model):
+    dte = models.DateField()
+    no = models.IntegerField()
+    check_no = models.IntegerField()
+    asa_no = models.CharField(max_length=30)
+    payee = models.CharField(max_length=255)
+    nature_of_transaction = models.CharField(max_length=255)
+    amountNetOfTax = models.DecimalField(decimal_places=3, max_digits=14)
+    grossAmount = models.DecimalField(decimal_places=3, max_digits=14)
+
+class ASDI_LFPS_AccountingEntriesRecord(models.Model):
+    lfps_dv = models.ForeignKey(ASDI_LFPS_DisbursmentVoucherRecord, on_delete=models.CASCADE)
+    account_title = models.CharField(max_length=255)
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
+
+
+class ASDI_COB_DisbursmentVoucherRecord(models.Model):
+    dte = models.DateField()
+    no = models.IntegerField()
+    check_no = models.IntegerField()
+    asa_no = models.CharField(max_length=30)
+    payee = models.CharField(max_length=255)
+    nature_of_transaction = models.CharField(max_length=255)
+    amountNetOfTax = models.DecimalField(decimal_places=3, max_digits=14)
+    grossAmount = models.DecimalField(decimal_places=3, max_digits=14)
+
+class ASDI_COB_AccountingEntriesRecord(models.Model):
+    lfps_dv = models.ForeignKey(ASDI_LFPS_DisbursmentVoucherRecord, on_delete=models.CASCADE)
+    account_title = models.CharField(max_length=255)
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
+
+
+class ASDI_CARP_DisbursmentVoucherRecord(models.Model):
+    dte = models.DateField()
+    no = models.IntegerField()
+    check_no = models.IntegerField()
+    asa_no = models.CharField(max_length=30)
+    payee = models.CharField(max_length=255)
+    nature_of_transaction = models.CharField(max_length=255)
+    amountNetOfTax = models.DecimalField(decimal_places=3, max_digits=14)
+    grossAmount = models.DecimalField(decimal_places=3, max_digits=14)
+
+class ASDI_CARP_AccountingEntriesRecord(models.Model):
+    lfps_dv = models.ForeignKey(ASDI_LFPS_DisbursmentVoucherRecord, on_delete=models.CASCADE)
+    account_title = models.CharField(max_length=255)
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
+
+
+# SDN MODELS -----------------------------------------------------------------------------------
+class SDN_LFPS_DisbursmentVoucherRecord(models.Model):
+    dte = models.DateField()
+    no = models.IntegerField()
+    check_no = models.IntegerField()
+    asa_no = models.CharField(max_length=30)
+    payee = models.CharField(max_length=255)
+    nature_of_transaction = models.CharField(max_length=255)
+    amountNetOfTax = models.DecimalField(decimal_places=3, max_digits=14)
+    grossAmount = models.DecimalField(decimal_places=3, max_digits=14)
+
+class SDN_LFPS_AccountingEntriesRecord(models.Model):
+    lfps_dv = models.ForeignKey(SDN_LFPS_DisbursmentVoucherRecord, on_delete=models.CASCADE)
+    account_title = models.CharField(max_length=255)
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
+
+
+class SDN_COB_DisbursmentVoucherRecord(models.Model):
+    dte = models.DateField()
+    no = models.IntegerField()
+    check_no = models.IntegerField()
+    asa_no = models.CharField(max_length=30)
+    payee = models.CharField(max_length=255)
+    nature_of_transaction = models.CharField(max_length=255)
+    amountNetOfTax = models.DecimalField(decimal_places=3, max_digits=14)
+    grossAmount = models.DecimalField(decimal_places=3, max_digits=14)
+
+class SDN_COB_AccountingEntriesRecord(models.Model):
+    lfps_dv = models.ForeignKey(SDN_LFPS_DisbursmentVoucherRecord, on_delete=models.CASCADE)
+    account_title = models.CharField(max_length=255)
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
+
+
+class SDN_CARP_DisbursmentVoucherRecord(models.Model):
+    dte = models.DateField()
+    no = models.IntegerField()
+    check_no = models.IntegerField()
+    asa_no = models.CharField(max_length=30)
+    payee = models.CharField(max_length=255)
+    nature_of_transaction = models.CharField(max_length=255)
+    amountNetOfTax = models.DecimalField(decimal_places=3, max_digits=14)
+    grossAmount = models.DecimalField(decimal_places=3, max_digits=14)
+
+class SDN_CARP_AccountingEntriesRecord(models.Model):
+    lfps_dv = models.ForeignKey(SDN_LFPS_DisbursmentVoucherRecord, on_delete=models.CASCADE)
+    account_title = models.CharField(max_length=255)
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
